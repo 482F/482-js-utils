21b99360d2ea61a6a5496ecaca3e6c598f18748f
482F <482F.git.mail@482f.net>
2022-04-07 00:00:00 +0900

    ディレクトリ構造を変更

--- index.js
+++ index.js
@@ -1,39 +1,9 @@
 const f = {}
-f.sleep = async (ms = 10) =>
-  await new Promise((resolve) => setTimeout(resolve, ms))
-f.wait = async (func, waitTime, timeout) => {
-  let timeouted = false
-  if (timeout) {
-    setTimeout(() => (timeouted = true), timeout)
-  }
-  let result
-  while (!(result = await func()) && !timeouted) {
-    await f.sleep(waitTime)
-  }
-  return result
-}
 
-f.filteringByHistory = (list, historyName, converter = (i) => i) => {
-  const fs = require('fs')
-  fs.appendFileSync(historyName, '', 'utf8')
-  const histories = fs.readFileSync(historyName, 'utf8').split('\n')
-  const filteredList = []
-  let appendText = ''
+const common = require('./modules/common.js')
+Object.assign(f, common)
 
-  for (const item of list) {
-    const convertedItem = converter(item).replaceAll(/[\r\n]/g, '')
-    if (histories.includes(convertedItem)) {
-      continue
-    } else {
-      filteredList.push(item)
-      appendText += convertedItem + '\n'
-    }
-  }
-  fs.appendFileSync(historyName, appendText, 'utf8')
-  return filteredList
-}
 
 module.exports = {
   ...f,
 }
-
