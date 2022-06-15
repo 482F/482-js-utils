0419c4a268654fac8b88036c6f1d9b217bcb2b73
482F <482F.git.mail@482f.net>
2022-06-16 00:00:00 +0900

    ディレクトリ構造を変更

---
+++ common.js
@@ -0,0 +1,44 @@
+export async function sleep(ms = 10) {
+  return await new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export async function wait(func, waitTime, timeout) {
+  let timeouted = false
+  if (timeout) {
+    setTimeout(() => (timeouted = true), timeout)
+  }
+  let result
+  while (!(result = await func()) && !timeouted) {
+    await sleep(waitTime)
+  }
+  return result
+}
+
+export async function clip(data) {
+  const blobs = Object.fromEntries(
+    Object.entries(data).map(([type, body]) => [
+      type,
+      new Blob([body], { type }),
+    ])
+  )
+  const clipboardItem = new ClipboardItem(blobs)
+  await navigator.clipboard.write([clipboardItem])
+}
+
+export async function clipText(text) {
+  return await clip({ 'text/plain': text })
+}
+
+export function download(fileName, datum) {
+  const a = document.createElement('a')
+  a.download = fileName
+  const [[type, body]] = Object.entries(datum)
+  const url = URL.createObjectURL(new Blob([body], { type }))
+  a.href = url
+  a.click()
+  URL.revokeObjectURL(url)
+}
+
+export function downloadText(fileName, text) {
+  return download(fileName, { 'text/plain': text })
+}
--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const f = {}
-
-import common from './modules/common.js'
-Object.assign(f, common)
-
-import textarea from './modules/textarea.js'
-Object.assign(f, textarea)
-
-export default { ...f }
+export * as common from './common.js'
+export * as textarea from './textarea.js'
