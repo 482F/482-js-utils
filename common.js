2a01aa32166c9e6164c6e339604fe5a8368c3cb4
482F <482F.git.mail@482f.net>
2022-10-18 00:00:00 +0900

    日付を文字列にフォーマットする関数の追加

--- common.js
+++ common.js
@@ -52,3 +52,51 @@ export function toAsyncCallback(func) {
   })
 }
 
+/**
+ * @param {Date} date - 変換対象の Date
+ * @param {String} format - 変換規則。下記の規則で変換される
+ *  - $yyyy -> 西暦
+ *  - $MM   -> 月
+ *  - $dd   -> 日
+ *  - $HH   -> 時
+ *  - $mm   -> 分
+ *  - $ss   -> 秒
+ *  - $fff  -> ミリ秒
+ *  - $a    -> 曜日 (ex. 月)
+ *  - $$    -> $ (エスケープ)
+ * @return {String} - date を format に従って変換した文字列
+ */
+export function formatDate(date, format) {
+  const p = (num) => num.toString().padStart(2, '0')
+  const rules = [
+    { target: 'yyyy', converter: (date) => date.getFullYear() },
+    { target: 'MM', converter: (date) => p(date.getMonth() + 1) },
+    { target: 'dd', converter: (date) => p(date.getDate()) },
+    { target: 'HH', converter: (date) => p(date.getHours()) },
+    { target: 'mm', converter: (date) => p(date.getMinutes()) },
+    { target: 'ss', converter: (date) => p(date.getSeconds()) },
+    { target: 'fff', converter: (date) => p(date.getMilliseconds()) },
+    { target: 'a', converter: (date) => '日月火水木金土'[date.getDay()] },
+  ].map(({ target, converter }) => ({
+    target,
+    pattern: new RegExp(`(?<!\\$)\\$${target}`, 'g'),
+    converter,
+  }))
+  const results = {}
+
+  return rules
+    .reduce((prev, { target, pattern, converter }) => {
+      return prev.map((section) => {
+        if (!section.match(pattern)) {
+          return section
+        }
+
+        if (!results[target]) {
+          results[target] = converter(date)
+        }
+
+        return section.replaceAll(pattern, results[target])
+      })
+    }, format.split('$$'))
+    .join('$')
+}
